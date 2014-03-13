ea53b2b8be09de78fc57af772448b286d30a0b6e
Franck Ernewein <franck.ernewein@gmail.com>
2014-03-13 20:11:33 +0100

    redefine UMD

--- tap.js
+++ tap.js
@@ -1,23 +1,19 @@
-(function (root, Library) {
-    // The square bracket notation is used to avoid property munging by the Closure Compiler.
-   if (typeof define == "function" && typeof define.amd == "object" && define.amd ) {
-     // Export for asynchronous module loaders (e.g., RequireJS, `curl.js`).
-     define(["exports"], Library);
-   } else {
-     // Export for CommonJS environments, web browsers, and JavaScript engines.
-     Library = Library(typeof exports == 'object' && exports || (root.Library = {
-       "noConflict": (function (original) {
-         function noConflict() {
-           root.Library = original;
-           // `noConflict` can't be invoked more than once.
-           delete Library.noConflict;
-           return Library;
-         }
-         return noConflict;
-       })(root.Library)
-     }));
-   }
- })(this, function (exports) {
+(function (root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+    //AMD. Register as an anonymous module.
+    define([], function () {
+      return factory({});
+    });
+  } else if (typeof exports === 'object') {
+    // CommonJS
+    factory(exports);
+  } else {
+    // Browser globals
+    factory((root.tap = {}));
+  }
+
+ }( this, function (exports) {
 
   function Line( string ) {
     this.string = string || ''; 
@@ -116,4 +112,4 @@
    exports.Parser = Parser;
 
    return exports;
- });
+ }));
